f51267e4465e3ffffaed9a6eb9237adfeb97db74
KrisTovski <kfilak@onet.eu>
2021-12-05 14:58:10 +0100

    add Liquibase

--- src/main/resources/db/version/01-create-tables.sql
+++ src/main/resources/db/version/01-create-tables.sql
@@ -1,9 +1,11 @@
+--liquibase formatted sql
+--changeset kris:1
 CREATE TABLE IF NOT EXISTS TOKEN (
                                     id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
                                     create_time DATETIME,
                                     access_token VARCHAR(1100)
 );
-
+--changeset kris:2
 CREATE TABLE IF NOT EXISTS SEAUNIT (
                                        id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
                                        created DATETIME,
